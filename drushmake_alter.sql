c0267c2f6d4f6decad62ed1d77aa206f3fb6fe79
Chris Ruppel <chris@fourkitchens.com>
2011-02-01 00:33:39 -0600

    added semi-colons alter SQL statements

--- drushmake_alter.sql
+++ drushmake_alter.sql
@@ -1,11 +1,9 @@
 /*
   NULL allowed for the version field
 */
-ALTER TABLE  `projects` CHANGE  `version`  `version` VARCHAR( 255 )
+ALTER TABLE  `projects` CHANGE  `version`  `version` VARCHAR( 255 );
 
 /*
   Each release can have a URL. This is for libraries and other non-contrib projects.
 */
-ALTER TABLE  `versions` ADD  `url` TEXT NULL AFTER  `release`
-
-/* -------------------------------------------------------------------------------- */
+ALTER TABLE  `versions` ADD  `url` TEXT NULL AFTER  `release`;
