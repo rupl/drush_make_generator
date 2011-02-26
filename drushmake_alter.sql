7023e8affb65deffd19e30f5a376695dff1817d7
Chris Ruppel <chris@fourkitchens.com>
2011-02-26 17:46:29 -0600

    updated SQL files to be current with code

--- drushmake_alter.sql
+++ drushmake_alter.sql
@@ -1,9 +1,19 @@
 /*
-  NULL allowed for the version field
-*/
+ * NULL allowed for the version field
+ */
 ALTER TABLE  `projects` CHANGE  `version`  `version` VARCHAR( 255 );
 
 /*
-  Each release can have a URL. This is for libraries and other non-contrib projects.
-*/
+ * Each release can have a URL. This is for libraries and other non-contrib projects.
+ */
 ALTER TABLE  `versions` ADD  `url` TEXT NULL AFTER  `release`;
+
+/*
+ * Add table for short URLs
+ */
+CREATE TABLE `aliases` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `makefile` INT NOT NULL ,
+  `alias` VARCHAR( 255 ) NOT NULL ,
+  UNIQUE (`alias`)
+) ENGINE = MYISAM ;
