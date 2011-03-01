3833919a6339bc0b9383412c46580cdef61356e4
Chris Ruppel <chris@fourkitchens.com>
2011-03-01 16:27:50 -0600

    people need to standardize their freakin .info files\!

--- update.php
+++ update.php
@@ -113,7 +113,7 @@ include('_lib.php');
     }
     
     // MODULE
-    else if (preg_match('/core\s*?=/',$projectInfo)) {
+    else if (preg_match('/name\s*?=/',$projectInfo)) {
 
       // now we parse the .info file for the goods
       print "   -- module .info file found";
