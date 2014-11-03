0b24086e9f9e1e0f20b7a41566415bb0ebeb5c0a
Chris Ruppel <chris.ruppel@gmail.com>
2014-11-03 05:29:42 -0800

    Add the footer back in.

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
   header("HTTP/1.0 503 Service Unavailable");
+
+  define('ANALYTICS_ACCOUNT', 'UA-432773-12');
 ?>
 <!doctype html>
 <html lang="en-US">
@@ -42,5 +44,28 @@
     <p>The source code has and always will be free, so you can set up your own instance: <a href="https://github.com/rupl/drush_make_generator">Code hosted on GitHub.</a></p>
     <p>oo00OO0000ooooorrrrr..... <a href="https://github.com/mattgrill/drushmakejs">contribute here</a> and we'll get a new, improved version running eventually.</p>
   </div>
+
+  <footer class="grid_12 vcard">
+    <p class="github">Source code on <a href="https://github.com/rupl/drush_make_generator" title="drush_make_generator on github">github</a></p>
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a>
+    <a class="fn org url" href="http://fourkitchens.com" title="Four Kitchens">Powered by Four Kitchens</a>
+  </footer>
+
+<?php if (ANALYTICS_ACCOUNT != ''): ?>
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '<?php print ANALYTICS_ACCOUNT; ?>']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  </script>
+<?php endif; ?>
+
 </body>
 </html>
